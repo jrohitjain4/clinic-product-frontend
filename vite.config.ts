dc838889d582beac2f565d11bdd2a8a34e3e67fb
Rohit Jain <rohitjainltp59@gmail.com>
2026-06-01 11:20:18 +0530

    chnge

--- vite.config.ts
+++ vite.config.ts
@@ -6,23 +6,5 @@ export default defineConfig({
   plugins: [react()],
   build: {
     chunkSizeWarningLimit: 2000,
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            if (id.includes('@remix-run') || id.includes('react-router')) {
-              return 'router';
-            }
-            if (id.includes('react') || id.includes('react-dom')) {
-              return 'react';
-            }
-            if (id.includes('tabler-icons') || id.includes('bootstrap-icons')) {
-              return 'icons';
-            }
-            return 'vendor'; // all other node_modules
-          }
-        },
-      },
-    },
   },
 })
